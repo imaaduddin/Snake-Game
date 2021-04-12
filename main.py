cf43ef3905c162e6bf0c8d1f4cadc4845a0d2919
Imaad Uddin <imaaduddin@icloud.com>
2021-04-11 22:54:00 -0500

    set background color

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pygame
+from pygame.locals import *
 
 
 
 if __name__ == "__main__":
   pygame.init()
 
-  surface = pygame.display.set_mode((500, 500))
+  surface = pygame.display.set_mode((1000, 500))
+  surface.fill((73, 179, 101))
   pygame.display.flip()
   
+  running = True
+
+  while running:
+    for event in pygame.event.get():
+      if event.type == KEYDOWN:
+        pass
+      elif event.type == QUIT:
+        running = False
