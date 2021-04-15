928f9aa182ad0f062b64b764affdf4431358aa3e
Imaad Uddin <imaaduddin@icloud.com>
2021-04-15 17:43:20 -0500

    rearrange code

--- main.py
+++ main.py
@@ -3,58 +3,52 @@ from pygame.locals import *
 
 class Game:
   def __init__(self):
-    pass
+    pygame.init()
+    self.snake = Snake()
+    self.snake.draw()
 
   def run(self):
-    pass
+    surface = pygame.display.set_mode((1000, 500))
+    running = True
 
-def draw_block():
-  surface.fill((73, 179, 101))
-  surface.blit(block, (block_x, block_y))
-  pygame.display.flip()
+    while running:
+      for event in pygame.event.get():
+        if event.type == KEYDOWN:
+          if event.key == K_ESCAPE:
+            running = False
+
+          if event.key == K_UP:
+            block_y = block_y - 10
+          
+
+          if event.key == K_DOWN:
+            block_y = block_y + 10
+          
+
+          if event.key == K_LEFT:
+            block_x = block_x - 10
+          
+
+          if event.key == K_RIGHT:
+            block_x += 10
+          
+        elif event.type == QUIT:
+          running = False
+
+
+class Snake:
+  def __init__(self, parent_screen):
+    self.parent_screen = parent_screen
+    self.block = pygame.image.load("resources/block.jpg").convert()
+    self.x = 100
+    self.y = 100
+
+  def draw(self):
+    self.parent_screen.fill((73, 179, 101))
+    self.parent_screen.blit(self.block, (self.x, self.y))
+    pygame.display.flip()
 
 
 if __name__ == "__main__":
   game = Game()
   game.run()
-  pygame.init()
-
-  surface = pygame.display.set_mode((1000, 500))
-  surface.fill((73, 179, 101))
-
-  block = pygame.image.load("resources/block.jpg").convert()
-  block_x = 100
-  block_y = 100
-  surface.blit(block, (block_x, block_y))
-
-
-  pygame.display.flip()
-  
-  running = True
-
-  while running:
-    for event in pygame.event.get():
-      if event.type == KEYDOWN:
-        if event.key == K_ESCAPE:
-          running = False
-
-        if event.key == K_UP:
-          block_y = block_y - 10
-          draw_block()
-
-        if event.key == K_DOWN:
-          block_y = block_y + 10
-          draw_block()
-
-        if event.key == K_LEFT:
-          block_x = block_x - 10
-          draw_block()
-
-        if event.key == K_RIGHT:
-          block_x += 10
-          draw_block()
-
-
-
-      elif event.type == QUIT:
-        running = False
